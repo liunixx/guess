a4f97bfbfa8bd4ee5f1bfe9c1c056260613df1a4
shajiawang <khliu.tw@gmail.com>
2020-05-21 23:12:13 +0800

    initial commit 20200521

---
+++ app/src/main/java/tw/khliu/guess/MainActivity.kt
@@ -0,0 +1,36 @@
+package tw.khliu.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+   val secretNumber = SecretNumber()
+   val TAG:String=MainActivity::class.java.simpleName
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Secret Number is : "+secretNumber.secret)
+    }
+
+   fun check(view: View) {
+       val n:Int = et_number.text.toString().toInt()
+       var msg = getString(R.string.yes_you_got_it)
+       val diff = secretNumber.validate(n)
+       if(diff<0) {
+           msg=getString(R.string.bigger)
+       } else if(diff>0) {
+           msg=getString(R.string.smaller)
+       }
+       // Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+       AlertDialog.Builder(this)
+           .setTitle(getString(R.string.dialog_title))
+           .setMessage(msg)
+           .setPositiveButton(getString(R.string.ok),null)
+           .show()
+   }
+}
---
+++ app/src/main/java/tw/khliu/guess/SecretNumber.kt
@@ -0,0 +1,19 @@
+package tw.khliu.guess
+
+import java.util.*
+
+class SecretNumber {
+    var secret:Int=Random().nextInt(10)+1
+    var count:Int  = 0
+
+    fun validate(number : Int) :Int{
+        count++
+        return number-secret
+    }
+}
+
+fun main(){
+    val secretNumber = SecretNumber()
+    println(secretNumber.secret)
+    println("${secretNumber.validate(2)}, count :${secretNumber.count}")
+}
